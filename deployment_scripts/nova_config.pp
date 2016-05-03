b39151975b73d799c66a97fc3e9e886ba21ffbd7
root <root@nailgun.test.domain.local>
2016-05-03 11:50:18 +0000

    new tasks.yaml

--- deployment_scripts/nova_config.pp
+++ deployment_scripts/nova_config.pp
@@ -1,12 +1,19 @@
 notice('MODULAR: glance_net_nova_config')
 $network_metadata   = hiera_hash('network_metadata')
 $glance_endpoint    = $network_metadata['vips']['glance']['ipaddr']
+$services = ['nova-api', 'cinder-api']
 
-service{ 'nova-api':
+service{ $services:
     ensure => running,
 }
 nova_config {
-    'glance/api_servers':  value  => $glance_endpoint ,
+    'glance/api_servers':  value  => '$glance_endpointi:9292',
 }
+cinder_config {
+    'DEFAULT/glance_api_servers':  value  => '$glance_endpoint:9292',
+}
+
+Cinder_config <||> ~> Service['cinder-api']
 Nova_config <||> ~> Service['nova-api']
+
 
