a2a37225918b3ffe6267cf38591dd68b95998062
root <root@nailgun.test.domain.local>
2016-05-05 07:55:42 +0000

    syntax correction

--- deployment_scripts/nova_config.pp
+++ deployment_scripts/nova_config.pp
@@ -1,19 +1,19 @@
 notice('MODULAR: glance_net_nova_config')
 $network_metadata   = hiera_hash('network_metadata')
-$glance_endpoint    = $network_metadata['vips']['glance']['ipaddr']
+$glance_endpoint_ip    = $network_metadata['vips']['glance']['ipaddr']
+$glance_endpoint_port  = '9292'
 $services = ['nova-api', 'cinder-api']
 
 service{ $services:
     ensure => running,
 }
 nova_config {
-    'glance/api_servers':  value  => $glance_endpoint':9292',
+    'glance/api_servers':  value  => "${glance_endpoint_ip}:${glance_endpoint_port}",
 }
 cinder_config {
-    'DEFAULT/glance_api_servers':  value  => $glance_endpoint':9292',
+    'DEFAULT/glance_api_servers':  value  => "${glance_endpoint_ip}:${glance_endpoint_port}",
 }
 
 Cinder_config <||> ~> Service['cinder-api']
 Nova_config <||> ~> Service['nova-api']
-
 
